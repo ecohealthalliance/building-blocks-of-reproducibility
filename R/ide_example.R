7be86799c6a3b0599901da48526043b1576f74d5
Collin Schwantes <schwantes@ecohealthalliance.org>
2022-05-16 17:31:58 -0400

    added some useful tips for the IDE section

---
+++ R/ide_example.R
@@ -0,0 +1,24 @@
+
+# This is poorly formatted code - use cmd+i to fix the indentation. 
+df_indent <- data.frame(a = 1:100,
+b="Some Text")
+
+# Click inside the function and use the tab button to see function arguments
+df_func <- data.frame(a = 1:10, row.names = FALSE)
+
+# Use cmd+c to comment out code and to uncomment it
+commented <- function(x){sprintf("an example %s",x)}
+
+# Use dashes to create code sections ----
+
+"All of this can be hidden with sections"
+
+# Next Section ----
+
+"Quickly jump between sections"
+
+# What other Rstudio conveniences do you like? 
+
+
+
+
